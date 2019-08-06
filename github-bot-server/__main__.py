02cd98f69206f2d6d17e472bd25f0f6bea2c3aac
LiuMengzhou <liumengzhou@xiaomi.com>
2019-08-06 18:07:15 +0800

    update

--- github-bot-server/__main__.py
+++ github-bot-server/__main__.py
@@ -17,6 +17,15 @@ secret = "dHMgaXMgYSBwaXR5"
 # 机器人的 github personal token
 oauth_token = base64.b64decode(
     "YWE5ZGNmMGYzMWQ2ZDI5NzZiNmMzMjQwMGIzMWFjNzk4YjY0MzEyNg==").decode("utf-8")
+# 模板必需填写的字段
+requiredLabels = [
+    "是否为新品（必填）",
+    "项目ID（必填）",
+    "企业名称（必填）",
+    "环境（必填）",
+    "现象（必填）",
+    "期望（必填）"
+]
 
 # 开发者新建 issue
 @router.register("issues", action="opened")
@@ -29,16 +38,15 @@ async def issue_opened_event(event, gh, *args, **kwargs):
     body = event.data["issue"]["body"]
 
     # 是不是按照模板提 issue
-    isIssueTemplateMatched = body.find(
-        "是否为新品") != -1 and body.find("填 新品 or 在售") == -1
+    isIssueTemplateMatched = all(body.find(label) for label in requiredLabels)
 
     if isIssueTemplateMatched:
         message = f"@{author} 感谢您提出宝贵的 issue，我会通知开发尽快处理！"
         await gh.post(comments_url, data={"body": message})
     else:
-        message = f"@{author} 感谢您提出宝贵的 issue\n但好像您并没有按照模板提出 issue，希望您能填写所有的必填项\n该 issue 将被 close！"
+        message = f"@{author} 感谢您提出宝贵的 issue\n但好像您并没有按照模板提出 issue，希望您能填写所有的必填项\n如果不修改，我们会降低此 issue 的优先级！"
         await gh.post(comments_url, data={"body": message})
-        await gh.patch(url, data={"state": "closed"})
+        # await gh.patch(url, data={"state": "closed"})
 
 
 @routes.post("/")
